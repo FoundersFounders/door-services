23a7bb6ffc4f348008b24b8aab69a3f6323ed012
Rui Gonçalves <ruippeixotog@gmail.com>
2016-03-11 21:04:30 +0000

    Monitor the STA_GOTIP event instead of polling for waiting for an IP

--- door-open-service/esp8266-nodemcu/init.lua
+++ door-open-service/esp8266-nodemcu/init.lua
@@ -13,16 +13,18 @@ gpio.write(gpio_pin, gpio.LOW)
 
 -- 3. Set-up timer
 function setup_alarm()
-  tmr.alarm(0, 3000, 1, function ()
-    local ip = wifi.sta.getip()
-    if ip then
-      tmr.stop(0)
+  if wifi.sta.getip() then
+    connect_to_sf_service()
+  else
+    configure_wifi()
+    wifi.sta.eventMonReg(wifi.STA_GOTIP, function()
+      wifi.sta.eventMonStop()
       connect_to_sf_service()
-    else
-      configure_wifi()
-    end
-  end)
+    end)
+    wifi.sta.eventMonStart()
+  end
 end
+
 setup_alarm()
 
 function connect_to_sf_service()
