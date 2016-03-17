994c55474d45bceb0168c40e8194c143626be9b6
DDuarte <dnpd.dd@gmail.com>
2016-03-17 12:38:44 +0000

    Major overhaul to the node-backend
    - Implement a simple HTTP API to open the door and retrieve stats, powered by hapi.js
    - Replace the low level RTM Slack module by SlackBots
    - Replace some hardcoded SQL queries by an ORM*, powered by Sequelize
    - Split the code in multiple files (app, backend and bellServer)
    - Slack channel and bot identifiers were replaced by lookups of their names
    
    * The table schema was not changed

--- door-open-service/node-backend/app.js
+++ door-open-service/node-backend/app.js
@@ -1,152 +1,153 @@
-var StringDecoder = require('string_decoder').StringDecoder;
-var decoder = new StringDecoder('utf8');
-var sqlite3 = require('sqlite3').verbose();
-var histogram = require('ascii-histogram');
-net = require('net');
+'use strict';
 
-var clients = [];
+const Hapi = require('hapi');
+const Joi = require('joi');
+const Boom = require('boom');
+const histogram = require('bars');
+const moment = require('moment');
+const Promise = require('bluebird');
+const Good = require('good');
+const SlackBot = require('slackbots');
+const net = require('net');
+const _ = require('underscore');
+const BellServer = require('./bellServer');
+const backend = require('./backend');
 
-net.createServer(function (socket) {
+const CHANNEL = '<CHANNEL_NAME>';
+const SLACK_TOKEN = '<SLACK_TOKEN>';
+const BOT_NAME = '<DOOR_BOT_NAME>';
+const SECRET = 'secret';
+const BELL_SERVER_PORT = 8300;
+const API_SERVER_PORT = 3000;
 
-  // Identify this client
-  socket.name = socket.remoteAddress + ":" + socket.remotePort;
+const server = new Hapi.Server();
+server.connection({ port: API_SERVER_PORT });
 
-  // Put this new client in the list
-  clients.push(socket);
-  console.log("New connection:");
-  console.log(socket);
-
-  // Handle incoming messages from clients.
-  socket.on('data', function (data) {
-    console.log(decoder.write(data));
-  });
-
-  // Remove the client from the list when it leaves
-  socket.on('end', function () {
-    console.log("end");
-    var clientIndex = clients.indexOf(socket);
-    if (clientIndex != -1) {
-      clients.splice(clientIndex, 1);
-    }
-  });
-
-  socket.on("error", function (err) {
-    console.log("Caught flash policy server socket error: ");
-    console.log(err.stack);
-    var clientIndex = clients.indexOf(socket);
-    if (clientIndex != -1) {
-      clients.splice(clientIndex, 1);
-    }
-  });
-
-  socket.on("close", function () {
-    console.log("Caught close.");
-    var clientIndex = clients.indexOf(socket);
-    if (clientIndex != -1) {
-      clients.splice(clientIndex, 1);
-    }
-  });
-}).listen(8300);
-
-// Send a message to all clients
-function broadcast(message) {
-  clients.forEach(function (client) {
-    // Don't want to send it to sender
-    client.write(message);
-  });
-  // Log it to the server output too
-  process.stdout.write(message)
-}
-
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', function (chunk) {
-  broadcast(chunk);
+const bellServer = new BellServer({
+  port: BELL_SERVER_PORT
 });
 
-
-// Slack integration
-// ------------------------------------------------------------------
-var request = require('request');
-var RtmClient = require('@slack/client').RtmClient;
-var RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var STATS_DATABASE = "stats.db";
-var token = '<SLACK_TOKEN>';
-var doorbot = '<DOOR_BOT_ID>';
-var channel = '<CHANNEL_ID>';
-
-var SlackWebClient = require('@slack/client').WebClient;
-var web = new SlackWebClient(token, {});
-
-function getUserId(userId, userInfoCb) {
-  web.users.info(userId, userInfoCb);
-}
-
-var db = new sqlite3.Database(STATS_DATABASE);
-db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS door_opens(id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, email TEXT, timestamp TEXT DEFAULT CURRENT_TIMESTAMP);");
+// create a bot
+const bot = new SlackBot({
+    token: SLACK_TOKEN,
+    name: BOT_NAME
 });
-var days_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-var rtm = new RtmClient(token, {logLevel: 'debug'});
-rtm.start();
-
-rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
-  rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-    console.log(message);
-    if (message.channel == channel && message.text != null && message.text.match(doorbot) != null) {
-      if (message.text.match(/open/i) != null) {
-        if (clients.length > 0) {
-          getUserId(message.user, function userInfoCb(err, info) {
-            if (err) {
-              console.log('Error:', err);
-            } else {
-              rtm.sendMessage('Opening the door as requested by ' + info.user.name + ' (' + info.user.profile.email + ')...', channel);
-              db.run("INSERT INTO door_opens(user, email) VALUES(?, ?)", info.user.name, info.user.profile.email);
-              broadcast("2500");
-            }
-          });
+bot.on('message', rawData => {
+  console.log('bot: ' + JSON.stringify(rawData));
+  
+  if (rawData.type !== 'message') {
+    return;
+  }
+  
+  const text = rawData.text;
+  const channelId = rawData.channel;
+  const user = rawData.user;
+  
+  if (!text || !channelId) {
+    return;
+  }
+  
+  Promise.props({
+    channel: bot.getChannel(CHANNEL),
+    bot: bot.getUser(BOT_NAME)
+  }).then(data => {
+    if (channelId === data.channel.id && text && text.indexOf(bot.self.id) != -1) {
+      if (text.match(/open/i) != null) {
+        if (bellServer.available()) {
+          backend.openDoor(user, SECRET, bot, bellServer, CHANNEL, SECRET);
         } else {
-          rtm.sendMessage('The remote door opening service is not operational at the moment. Consider dispatching a drone to pick up a human.', channel);
+          bot.postMessageToChannel(CHANNEL, 'The remote door opening service is not operational at the moment. Consider dispatching a drone to pick up a human.');
         }
-      } else if (message.text.match(/stats/i) != null) {
-        db.get("SELECT COUNT(*) as count FROM door_opens", function(err, r1) {
-          if (err) {
-            console.log('Error:', err);
-          } else if (r1) {
-            db.get("SELECT timestamp FROM door_opens ORDER BY timestamp ASC LIMIT 1", function(err, r2) {
-              if (err) {
-                console.log('Error:', err);
-              } else if (r2) {
-                var msg = 'The remote door opening service was used ' + r1.count + ' time(s) since ' + r2.timestamp + '.';
-                var counts = {};
-                days_of_week.forEach(function(el) { counts[el] = 0; });
-                db.each("SELECT case cast(strftime('%w', timestamp) as integer)\
-                         when 0 then 'Sun'\
-                         when 1 then 'Mon'\
-                         when 2 then 'Tue'\
-                         when 3 then 'Wed'\
-                         when 4 then 'Thu'\
-                         when 5 then 'Fri'\
-                         else 'Sat' end as dayofweek, COUNT(*) as count FROM door_opens GROUP BY dayofweek", function(err, r3) {
-                           counts[r3.dayofweek] = r3.count;
-                         }, function() {
-                           msg += '\nBreakdown by day:';
-                           msg += '\n```';
-                           msg += histogram(counts, { sort: false });
-                           msg += '```';
-                           msg += '\nAssuming that it takes ~40 seconds to open the door and get back, around ' + Math.round((r1.count * 40)/60) + ' minutes have been saved!';
-                           rtm.sendMessage(msg, channel);
-                         });
-              }
-            });
-          } else {
-            rtm.sendMessage('There are no available stats for the remote door opening service', channel);
+      } else if (text.match(/stats/i) != null) {
+        return backend.getStats().then(stats => {
+          if (!stats) {
+            throw new Error('There are no available stats for the remote door opening service');
           }
+
+          const count = _.reduce(_.values(_.omit(stats, 'since')), (m, val) => m + val, 0);
+          const savedTime = Math.round((count * 40.0)/60.0);
+          const timeStr = moment(stats.since, moment.ISO_8601).fromNow();
+          let msg = 'The remote door opening service was used ' + count + ' time(s) since ' + timeStr + '.';
+          msg += '\nBreakdown by day:';
+          msg += '\n```';
+          msg += histogram(_.omit(stats, 'since'), { sort: false });
+          msg += '```';
+          msg += '\nAssuming that it takes ~40 seconds to open the door and get back, around ' + savedTime + ' minutes have been saved!';
+          bot.postMessageToChannel(CHANNEL, msg);
+        }).catch(err => {
+          console.log(err);
+          bot.postMessageToChannel(CHANNEL, err.message);
         });
       }
     }
+  }).catch(err => console.log(err));
+});
+
+// GET /ping
+server.route({
+  method: 'GET',
+  path: '/ping',
+  handler: (request, reply) => {
+    reply('I\'m alive.');
+  }
+});
+
+// GET /stats
+server.route({
+  method: 'GET',
+  path: '/stats',
+  handler: (request, reply) => {
+    backend.getStats()
+      .then(data => reply(data))
+      .catch(err => { console.log(err); reply(err); });
+  }
+});
+
+// POST /open
+server.route({
+  method: 'POST',
+  path: '/open',
+  handler: (request, reply) => {
+    backend.openDoor(request.payload.id, request.payload.secret, bot, bellServer, CHANNEL, SECRET).then(user => {
+      if (!user) {
+        reply(Boom.unauthorized());
+      } else {
+        reply(user);
+      }
+    }).catch(err => { console.log(err); reply(err) });
+  },
+  config: {
+    validate: {
+      payload: {
+        id: Joi.string(),
+        secret: Joi.string()
+      }
+    }
+  }
+});
+
+server.register({
+  register: Good,
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      events: {
+        response: '*',
+        log: '*'
+      }
+    }]
+  }
+}, err => {
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+
+    server.log('info', 'Server running at: ' + server.info.uri);
   });
 });
