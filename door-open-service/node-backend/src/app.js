4438d2a505ebdf80af91bb5953f7dcf5c82c56c9
Rui Gonçalves <ruippeixotog@gmail.com>
2016-05-23 18:00:54 +0100

    Use a dedicated config library

--- door-open-service/node-backend/src/app.js
+++ door-open-service/node-backend/src/app.js
@@ -1,17 +1,15 @@
-import fs from "fs";
-
 import DoorSlackBot from "./DoorSlackBot";
 import DoorSocketServer from "./DoorSocketServer";
 import startHttpInterface from "./httpInterface";
 import startSlackInterface from "./slackInterface";
 
-const config = JSON.parse(fs.readFileSync("config.json"));
+import config from "config";
 
-const sockServer = new DoorSocketServer(config.socketServer);
-const slackBot = new DoorSlackBot(config.slackBot);
+const sockServer = new DoorSocketServer(config.get("socketServer"));
+const slackBot = new DoorSlackBot(config.get("slackBot"));
 
-if (config.interfaces.slack.enabled)
-  startSlackInterface(config.interfaces.slack, slackBot, sockServer);
+if (config.get("interfaces.slack.enabled"))
+  startSlackInterface(config.get("interfaces.slack"), slackBot, sockServer);
 
-if (config.interfaces.http.enabled)
-  startHttpInterface(config.interfaces.http, slackBot, sockServer);
+if (config.get("interfaces.http.enabled"))
+  startHttpInterface(config.get("interfaces.http"), slackBot, sockServer);
